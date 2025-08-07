1229e3acb20de6d39cc23d5c6b6b7776b3e106d9
Sergio Sanz <sergios98@hotmail.com>
2025-08-07 18:18:58 +0200

    add profile page

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,9 +1,31 @@
+import { useAuth } from '@/hooks/use-auth'
+import { cn } from '@/lib/utils'
+
 export type ProfileProps = React.HTMLAttributes<HTMLDivElement> & {}
 
 export function Profile({ ...props }: ProfileProps) {
+  const { user } = useAuth()
+
+  if (!user) {
+    return (
+      <div className={cn('max-w-4xl mx-auto p-4')} {...props}>
+        <p>No user found</p>
+      </div>
+    )
+  }
+
   return (
-    <div {...props}>
-      <p>Profile</p>
+    <div className={cn('max-w-4xl mx-auto p-4 flex flex-col gap-4')} {...props}>
+      <img
+        className="size-24"
+        src={user.image}
+        alt={`${user.firstName} ${user.lastName}`}
+      />
+      <h1 className="text-3xl font-bold">
+        {user.firstName} {user.lastName}
+      </h1>
+
+      <span className="text-gray-900">Email: {user.email}</span>
     </div>
   )
 }
