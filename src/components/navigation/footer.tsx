a413c17d93a5acbb4dc0b7454e927109720bb4e2
Sergio Sanz <sergios98@hotmail.com>
2025-08-07 17:31:38 +0200

    add footer styles

--- src/components/navigation/footer.tsx
+++ src/components/navigation/footer.tsx
@@ -2,8 +2,12 @@ export type FooterProps = React.HTMLAttributes<HTMLDivElement> & {}
 
 export function Footer({ ...props }: FooterProps) {
   return (
-    <div {...props}>
-      <p>Footer</p>
-    </div>
+    <footer {...props}>
+      <div className="max-w-4xl mx-auto px-4 py-4 text-center text-sm text-gray-500">
+        <p>
+          &copy; {new Date().getFullYear()} Social Network. All rights reserved.
+        </p>
+      </div>
+    </footer>
   )
 }
